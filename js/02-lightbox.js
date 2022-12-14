7848f3105447ddd942c4f220ec2a804f397702d2
serheihimself <sergm080@gmail.com>
2022-12-14 13:46:20 +0200

    task2 commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,4 +7,16 @@ const imgTarget = document.querySelector('.gallery')
 const itemTarget = []
 
 galleryItems.forEach(elem => {
+    const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = elem.original
+	const galleryImg = document.createElement('img')
+	galleryImg.className = 'gallery__image'
+	galleryImg.src = elem.preview
+	galleryImg.setAttribute('title', elem.description)
+	galleryImg.alt = elem.description
+
+	galleryLink.append(galleryImg)
+	itemTarget.push(galleryLink)
 })
+imgTarget.append(...itemTarget)
