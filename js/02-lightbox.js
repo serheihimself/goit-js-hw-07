f28bb1c9712c039aceb1f9bb8cf5954ce532404e
serheihimself <sergm080@gmail.com>
2022-12-14 13:41:09 +0200

    task2_create-dom-elements-commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgTarget = document.querySelector('.gallery')
+const itemTarget = []
+
+galleryItems.forEach(elem => {
+})
